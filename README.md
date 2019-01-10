6ece3c2c77f789aefa5df0bef22a439c1091b623
Olga Minaeva <olelukoje@users.noreply.github.com>
2019-01-10 12:01:30 +0700

    Revert "Feature/input params as json"

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ### **Launch Instructions:**
 1. Build an image from a Dockerfile: `docker build -t wine_quality_image .`
-2. Derive a container from the image: `docker run -e MODEL_NAME='model_name.pkl' -p host_port:container_port wine_quality_image`
-3. Make curl to check: `curl -d input_json http://localhost:host_port/predict`
+2. Derive a container from the image: `docker run -p host_port:container_port wine_quality_image`
+3. Make curl to check: `curl -d ./wine_inputs.json http://localhost:host_port/predict`
 
--- predicts_api.py
+++ predicts_api.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask, request, jsonify
 import pickle
 import pandas as pd
+import json
 
 app = Flask(__name__)
 MODEL_NAME = os.environ['MODEL_NAME']
@@ -17,16 +18,18 @@ def start():
 def predict():
     if request.method == 'POST':
         try:
-            json_data = request.get_json()
-            if json_data is None:
-                return jsonify("Expected json")
-            else:
-                df = pd.DataFrame({0: json_data}).transpose()
+            json_path = request.get_data()
+            with open(json_path, "r") as f:
+                json_data = json.load(f)
+            df = pd.DataFrame({0: json_data}).transpose()
         except ValueError:
-            return jsonify("Please check the json format")
+            return jsonify("Please check the json file.")
 
         return jsonify(rf_model.predict(df).tolist())
 
 
 with open("models/" + MODEL_NAME, 'rb') as f:
     rf_model = pickle.load(f)
+
+
+
