f315a242bdabf799be69bfaff1f418170be9ed8b
olelukoje <minaeva.olja@gmail.com>
2019-01-11 16:34:39 +0700

    add BadRequest exception

--- predicts_api.py
+++ predicts_api.py
@@ -2,6 +2,8 @@ import os
 from flask import Flask, request, jsonify
 import pickle
 
+from werkzeug.exceptions import BadRequest
+
 app = Flask(__name__)
 MODEL_NAME = os.environ['MODEL_NAME']
 
@@ -21,41 +23,20 @@ def predict():
         try:
             json_data = request.get_json()
             if json_data is None:
-                raise InvalidRequestData("Expected json")
+                raise BadRequest("Expected json")
             else:
+                """Check input properties order"""
                 properties = [list(d.keys()) for d in json_data]
                 for name in properties:
                     if name == properties_name:
                         continue
                     else:
-                        raise InvalidRequestData("Incorrect properties order")
+                        raise BadRequest("Incorrect properties order")
                 data = [list(d.values()) for d in json_data]
         except ValueError:
-            raise InvalidRequestData("Invalid json format")
+            raise BadRequest("Invalid json format")
 
         return jsonify(rf_model.predict(data).tolist())
-
-
-class InvalidRequestData(Exception):
-    status_code = 400
-
-    def __init__(self, message, status_code=None):
-        Exception.__init__(self)
-        self.message = message
-        if status_code is not None:
-            self.status_code = status_code
-
-    def to_dict(self):
-        rv = dict()
-        rv['message'] = self.message
-        return rv
-
-
-@app.errorhandler(InvalidRequestData)
-def handle_invalid_request_exception(error):
-    response = jsonify(error.to_dict())
-    response.status_code = error.status_code
-    return response
 
 
 with open("models/" + MODEL_NAME, 'rb') as f:
