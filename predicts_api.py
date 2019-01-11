6d80e9e157ce7c6d1aff7ec171b94d8c45005278
olelukoje <minaeva.olja@gmail.com>
2019-01-11 12:20:55 +0700

    handle incorrect properties order

--- predicts_api.py
+++ predicts_api.py
@@ -5,6 +5,9 @@ import pickle
 app = Flask(__name__)
 MODEL_NAME = os.environ['MODEL_NAME']
 
+properties_name = ["fixed acidity", "volatile acidity", "citric acid", "residual sugar", "chlorides",
+                   "free sulfur dioxide", "total sulfur dioxide", "density", "pH", "sulphates", "alcohol"]
+
 
 @app.route("/", methods=['GET'])
 def start():
@@ -18,8 +21,15 @@ def predict():
         try:
             json_data = request.get_json()
             if json_data is None:
+                # TODO: add a response error code
                 return jsonify("Expected json")
             else:
+                properties = [list(d.keys()) for d in json_data]
+                for name in properties:
+                    if name == properties_name:
+                        continue
+                    else:
+                        return jsonify("Incorrect properties order")
                 data = [list(d.values()) for d in json_data]
         except ValueError:
             return jsonify("Please check the json format")
